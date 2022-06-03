1d9b8918220347b95033e5e32ddd832a485da72d
huseyinbatta <huseyinbattal42@gmail.com>
2022-06-03 23:42:05 +0300

    enter key

--- public/chat.js
+++ public/chat.js
@@ -1,9 +1,9 @@
 //herokuda kullanılacak port.
-const port = "https://live-chat-app-with-node.herokuapp.com/";  
+//const port = "https://live-chat-app-with-node.herokuapp.com/";  
 
 
 //Yerelde kullanılacak port.
-//const port = "http://localhost:3000/"; 
+const port = "http://localhost:3000/"; 
 
 const socket = io.connect(`${port}`);
 
@@ -20,16 +20,6 @@ submitBtn.addEventListener("click", () => {
     sender: sender.value,
   });
     sender.disabled = true;
-});
-
-message.addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-        socket.emit("chat", {
-            message: message.value,
-            sender: sender.value,
-          });
-            sender.disabled = true;
-    }
 });
 
 socket.on("chat", (data) => {
